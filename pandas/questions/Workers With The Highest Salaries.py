45dac3c69c63d12f1b7875b159372d49bda324f5
mynameislyonya <51920564+mynameislyonya@users.noreply.github.com>
2022-12-06 14:05:00 +0100

    Update and rename Workers With The Highest Salaries to Workers With The Highest Salaries.py

--- pandas/questions/Workers With The Highest Salaries.py
+++ pandas/questions/Workers With The Highest Salaries.py
@@ -1,16 +1,8 @@
-/*
 https://platform.stratascratch.com/coding/10353-workers-with-the-highest-salaries?code_type=2
-
-DoorDash
-Medium
-Interview Questions
-ID 10353
-*/
 
 Find the titles of workers that earn the highest salary. Output the highest-paid title or multiple titles that share the highest salary.
 
 import pandas as pd
-
 title.rename(columns={'worker_ref_id':'worker_id'},inplace=True)
 df = worker.merge(title,how='left',on='worker_id')
 df[df.salary == df.salary.max()]['worker_title']
