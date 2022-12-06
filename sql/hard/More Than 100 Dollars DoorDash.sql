7c2d17f1308feb4c12f1e6aa004d548d403b5e18
mynameislyonya <51920564+mynameislyonya@users.noreply.github.com>
2022-12-06 09:39:28 +0100

    Update More Than 100 Dollars DoorDash.sql

--- sql/hard/More Than 100 Dollars DoorDash.sql
+++ sql/hard/More Than 100 Dollars DoorDash.sql
@@ -7,28 +7,31 @@ For each month of 2021, calculate what percentage of restaurants, out of these t
 
 with tab as(
   select 
-     distinct date_part('month',order_placed_time) as dt,
-     sum(count(distinct restaurant_id)) over(partition by date_part('month',order_placed_time)) as s
+    distinct date_part('month',order_placed_time) as dt,
+    sum(count(distinct restaurant_id)) over(partition by date_part('month',order_placed_time)) as s
   from
     delivery_orders
-    left join order_value ov
-    on delivery_orders.delivery_id=ov.delivery_id
-   where to_char(order_placed_time, 'yyyy') = '2021'
-   group by date_part('month',order_placed_time),restaurant_id
-   having sum(sales_amount)>100),
+  left join
+    order_value ov
+  on
+    delivery_orders.delivery_id=ov.delivery_id
+  where to_char(order_placed_time, 'yyyy') = '2021'
+  group by date_part('month',order_placed_time),restaurant_id
+  having sum(sales_amount)>100),
 t as (
   select 
-     date_part('month',order_placed_time) as dt,
-     count(distinct restaurant_id) as c
+    date_part('month',order_placed_time) as dt,
+    count(distinct restaurant_id) as c
   from
     delivery_orders
-  left join order_value ov
-    on delivery_orders.delivery_id=ov.delivery_id
-    where to_char(order_placed_time, 'yyyy') = '2021'
-    group by date_part('month',order_placed_time)
+  left join
+    order_value ov
+  on
+    delivery_orders.delivery_id=ov.delivery_id
+  where to_char(order_placed_time, 'yyyy') = '2021'
+  group by date_part('month',order_placed_time)
 )
 
 select t.dt,s/c*100
-
 from tab left join t on tab.dt=t.dt
 
