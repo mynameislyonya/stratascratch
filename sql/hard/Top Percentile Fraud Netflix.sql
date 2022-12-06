a39578897dbf4d08cc2a93872fab6fd1dd8bc95f
mynameislyonya <51920564+mynameislyonya@users.noreply.github.com>
2022-12-06 10:02:36 +0100

    Create Top Percentile Fraud Netflix.sql

---
+++ sql/hard/Top Percentile Fraud Netflix.sql
@@ -0,0 +1,7 @@
+https://platform.stratascratch.com/coding/10303-top-percentile-fraud?tabname=question
+
+with percentiles as (
+select * , 
+percent_rank() over (partition by state order by fraud_score desc) as percentile from fraud_score)
+select policy_num, state, claim_cost, fraud_score from percentiles
+where percentile <= 0.05
